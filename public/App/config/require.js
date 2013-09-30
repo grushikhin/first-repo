b55bddfdfa3194ddf773e7cb5d7eaf101a2e34e5
grushikhin <grushikhin@gmail.com>
2013-09-30 10:01:32 +0400

    bower and gitignore

---
+++ public/App/config/require.js
@@ -0,0 +1,12 @@
+(function(root) {
+    'use strict';
+
+    root.require = {
+        baseUrl: "./",
+        paths: {
+            "jQuery": "vendor/jquery/jquery"
+        }
+    };
+
+
+}(window));
--- public/App/modules/index.js
+++ public/App/modules/index.js
@@ -1,7 +1,6 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: ¿Õﬁ“¿
- * Date: 30.09.13
- * Time: 9:47
- * To change this template use File | Settings | File Templates.
- */
+define([
+    'jQuery'
+], function ($) {
+    debugger;
+    return $;
+});
