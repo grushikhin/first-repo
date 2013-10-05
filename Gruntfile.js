465e4bb8c21e36c9f148728604a84263bc29ab5d
grushikhin <grushikhin@gmail.com>
2013-10-05 18:20:03 +0400

    'routes'

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+var fs = require('fs');
+
+module.exports = function(grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    hostname: 'grushikhin.local',
+                    port: 80,
+                    base: 'public',
+                    keepalive: true,
+                    middleware: function(connect, options) {
+                        return [
+                            connect.static(options.base),
+                            function(req, res, next) {
+                                fs.readFile(options.base + '/index.html', 'utf8', function(e, html) {
+                                    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+                                    res.end(html);
+                                });
+                            }
+                        ];
+                    }
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+};
